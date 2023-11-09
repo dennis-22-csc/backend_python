93bc287340bd0a8676be3007263c6759448636da
dennis-22-csc <dennisakpotaire.gmail.com>
2023-11-09 07:56:44 +0100

    refactored 0-insert_data to facilitate unit testing

--- sqlalchemy/0-insert_data.py
+++ sqlalchemy/0-insert_data.py
@@ -1,15 +1,38 @@
 """
 
-This module creates a new user and a corresponding profile in the database.
+This module creates a new user in a database.
+It contains a function that creates a new user in a database. 
+
+Functions:
+    create_user(session, user): Add a new user to the database.
 
 """
 
 from models import User, Profile
 from config import Session
 
-# Create a new user and a corresponding profile
-new_user = User(username='dennis', profile=Profile(full_name='Akpotaire Dennis'))
-session = Session()
-session.add(new_user)
-session.commit()
-session.close()
+def create_user(session, user):
+    """Create a new user in the database.
+    Args:
+        session (Session): The Session object for database interaction.
+        user (User): The new user to be added to the database.
+    
+    Returns:
+        An integer representing success or failure. 1 for success. -1 for failure.
+
+    Usage:
+    result = create_user(session, user)
+
+    """
+    try:
+        session.add(user)
+        session.commit()
+        return 1
+    except Exception:
+        return -1
+
+if __name__ == "__main__":
+    new_user = User(username='dennis', profile=Profile(full_name='Akpotaire Dennis'))
+    with Session() as db_session:
+        result = create_user(db_session, new_user)
+        print("result: ", result)
