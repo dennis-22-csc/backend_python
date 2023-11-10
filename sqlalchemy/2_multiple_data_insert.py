2ad1015b893b02c0e6317c32b29ee94e5b6bb7c8
dennis-22-csc <dennisakpotaire.gmail.com>
2023-11-10 09:30:50 +0100

    added unit tests for 2_multiple_data_insert module

--- sqlalchemy/2_multiple_data_insert.py
+++ sqlalchemy/2_multiple_data_insert.py
@@ -8,6 +8,9 @@ Functions:
 """
 
 from models import Customer
+import logging
+
+logging.basicConfig(filename='database.log', level=logging.INFO)
 
 def insert_customers_from_dict_list(session, customers_list):
     """Receives a list of dictionaries storing customer information and creates corresponding customers in the database.
@@ -20,14 +23,29 @@ def insert_customers_from_dict_list(session, customers_list):
 	    Usage:
 		    result = insert_customers_from_dict_list(db_session, customers)
     """
+    failure_count = 0
     try:
+        if not isinstance(customers_list, list):
+            raise ValueError("Injected customer data not in a list.")
         for customer_dict in customers_list:
-            customer = Customer(name=customer_dict['name'], age=customer_dict['age'])
-            session.add(customer)
+            if not isinstance(customer_dict, dict):
+                failure_count += 1
+                logging.info(f"{customer_dict} is not a dictionary.")
+                continue
+            try:
+                customer = Customer(name=customer_dict['name'], age=customer_dict['age'])
+                session.add(customer)
+            except KeyError:
+                failure_count += 1
+                logging.info(f"{customer_dict} doesn't have a correct key.")
+                continue
         session.commit()
-        return "Customers added successfully"
+        if failure_count == 0:
+            return "Customers added successfully"
+        else:
+            return f"Added all customers except {failure_count}. Check the database log for more details."
     except Exception as e:
-        return f"Error occured: {e}"
+        return f"Error occurred: {e}"
 
 if __name__ == "__main__":
     Session = __import__("config").Session
